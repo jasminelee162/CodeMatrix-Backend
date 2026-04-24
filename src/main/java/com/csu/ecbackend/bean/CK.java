74494bd7348bb6c10792c2d60d4fdc89e53f1f93
Hao-2005 <1972781375@qq.com>
2026-04-24 16:44:17 +0800

    补充了CK度量的WMC、RFC和LCOM计算

--- src/main/java/com/csu/ecbackend/bean/CK.java
+++ src/main/java/com/csu/ecbackend/bean/CK.java
@@ -5,7 +5,10 @@ import lombok.Data;
 @Data
 public class CK {
     private String name;
+    private int wmc;
+    private int rfc;
     private int dit;
     private int cbo;
     private int noc;
+    private int lcom;
 }
--- src/main/java/com/csu/ecbackend/util/remoteDBUtil.java
+++ src/main/java/com/csu/ecbackend/util/remoteDBUtil.java
@@ -7,7 +7,7 @@ import java.sql.*;
 public class remoteDBUtil {
     private remoteDBUtil() {
     }
-   private static String mySQLDriver="com.mysql.cj.jdbc.Driver";
+    private static String mySQLDriver="com.mysql.cj.jdbc.Driver";
     private static String masterUrl="jdbc:mysql://47.101.195.251:3306/redisDemo?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static String slaveUrl="jdbc:mysql://47.92.128.133:3316/redisDemo?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static  String user="root";
